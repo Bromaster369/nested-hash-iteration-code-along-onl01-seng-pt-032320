cf42b05dff59eb7270e1adcabaceed1f8051c7bf
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-02 01:17:17 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
+contact.each do |person, contact_details_hash|
   if person == "Fredy Mercury"
     contact_details_hash.each do |attribute, data|
      binding.pry
